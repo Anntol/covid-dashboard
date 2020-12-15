44ece66fd36ed4d223842e576d33864dbbd7768d
Anna Tolstova <anna_tolstova@hotmail.com>
2020-12-15 18:51:42 +0200

    feat: add footer component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,16 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { CovidService } from '../core/services/covid.service';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [PageNotFoundComponent],
+  declarations: [PageNotFoundComponent, FooterComponent],
   imports: [
     CommonModule,
     SharedModule
   ],
   providers: [CovidService],
-  exports: [PageNotFoundComponent],
+  exports: [PageNotFoundComponent, FooterComponent],
 })
 export class CoreModule { }
