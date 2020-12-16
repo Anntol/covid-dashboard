cbe287c0a93bdbfc515bc3095ec14d5f95840b69
coriander31415 <coriander31415@gmail.com>
2020-12-16 17:33:46 +0200

    feat: add header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,16 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { CovidService } from '../core/services/covid.service';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [PageNotFoundComponent],
+  declarations: [PageNotFoundComponent, HeaderComponent],
   imports: [
     CommonModule,
     SharedModule
   ],
   providers: [CovidService],
-  exports: [PageNotFoundComponent],
+  exports: [PageNotFoundComponent, HeaderComponent],
 })
 export class CoreModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -13,12 +15,16 @@ import { MatCardModule } from '@angular/material/card';
     MatIconModule,
     FlexLayoutModule,
     MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   exports: [
     CommonModule,
     MatIconModule,
     FlexLayoutModule,
     MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
   ]
 })
 export class SharedModule { }
