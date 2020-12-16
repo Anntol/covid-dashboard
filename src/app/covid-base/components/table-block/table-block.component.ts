73e5ab57c81207559dcf5fc6e66014a27b838416
Anna Tolstova <anna_tolstova@hotmail.com>
2020-12-16 22:32:57 +0200

    init: start table block implementation

---
+++ src/app/covid-base/components/table-block/table-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-block',
+  templateUrl: './table-block.component.html',
+  styleUrls: ['./table-block.component.scss']
+})
+export class TableBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/covid-base/covid-base.module.ts
+++ src/app/covid-base/covid-base.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { TestComponent } from './components/test/test.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { TableBlockComponent } from './components/table-block/table-block.component';
 
 @NgModule({
-  declarations: [TestComponent, HomePageComponent],
+  declarations: [TestComponent, HomePageComponent, TableBlockComponent],
   imports: [
     CommonModule,
     SharedModule
