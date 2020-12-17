ebb1fc76206856cedafe4bb6b370e63aa6138a3c
Anna Tolstova <anna_tolstova@hotmail.com>
2020-12-17 20:49:02 +0200

    feat: transpose table

--- src/app/covid-base/components/table-block/table-block.component.ts
+++ src/app/covid-base/components/table-block/table-block.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { ICovid19 } from 'src/app/core/models/covid-base.models';
 
 @Component({
@@ -6,10 +7,18 @@ import { ICovid19 } from 'src/app/core/models/covid-base.models';
   templateUrl: './table-block.component.html',
   styleUrls: ['./table-block.component.scss']
 })
+
 export class TableBlockComponent implements OnInit, OnChanges {
+  labels: string[] = ['Cases', 'Deaths', 'Recovered'];
+  dataColumns: string[] = ['cases', 'deaths', 'recovered'];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource;
+  displayColumns: string[] = [];
+  displayData: any[] = [];
+  inputData: any[] = [];
+  
+  @Input() covidData!: ICovid19;
 
   constructor() { }
-  @Input() covidData!: ICovid19;
 
   ngOnInit(): void {    
   }
@@ -17,19 +26,42 @@ export class TableBlockComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     // only run when property "covidData" changed
     if (changes['covidData']) {
-      this.dataSource  = [{
+      const globalData: ITableElement = {
         cases: this.covidData.Global.TotalConfirmed,
         deaths: this.covidData.Global.TotalDeaths,
         recovered: this.covidData.Global.TotalRecovered
-      }]
+      };
+      this.inputData = [globalData];
+
+      this.transpose();
+      this.fillLabels();
     }
+  } 
+
+  // idea from https://stackblitz.com/edit/angular-cjskob?file=src%2Fapp%2Ftable.component.ts
+  transpose() {
+    let transposedData: any[] = [];
+    for (let column = 0; column < this.dataColumns.length; column += 1) {
+      transposedData[column] = {
+        label: this.labels[column]
+      };
+      for (let row = 0; row < this.inputData.length; row += 1) {
+        transposedData[column][`column${row}`] = this.inputData[row][this.dataColumns[column]];
+      }
+    }
+    this.dataSource = new MatTableDataSource(transposedData);
   }
 
-  tableColumns:  string[] = ['cases', 'deaths', 'recovered'];
+  fillLabels() {
+    this.displayColumns = ['label'];
+    for (let i = 0; i < this.inputData.length; i++) {
+      this.displayColumns.push('column' + i);
+    }
+  }
+}
 
-  dataSource  = [{
-    cases: 0,
-    deaths: 0,
-    recovered: 0
-  }]
+export interface ITableElement {
+  cases: number;
+  deaths: number;
+  recovered: number;
 }
