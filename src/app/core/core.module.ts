4281397d7d29321806fe59b30fa3c98ba964a89c
coriander31415 <coriander31415@gmail.com>
2020-12-17 10:16:50 +0200

    feat: add recommend dialog

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,16 +5,18 @@ import { SharedModule } from '../shared/shared.module';
 import { CovidService } from '../core/services/covid.service';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DialogLikeComponent } from './components/dialog-like/dialog-like.component';
+import { DialogLikeContentComponent } from './components/dialog-like/dialog-like.component';
 
 
 
 @NgModule({
-  declarations: [PageNotFoundComponent, HeaderComponent],
+  declarations: [PageNotFoundComponent, HeaderComponent, DialogLikeComponent, DialogLikeContentComponent],
   imports: [
     CommonModule,
     SharedModule
   ],
   providers: [CovidService],
-  exports: [PageNotFoundComponent, HeaderComponent],
+  exports: [PageNotFoundComponent, HeaderComponent, DialogLikeComponent, DialogLikeContentComponent],
 })
 export class CoreModule { }
