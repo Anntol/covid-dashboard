7e6a096255c92353815c331aa37ffb734af72564
coriander31415 <coriander31415@gmail.com>
2020-12-19 15:38:54 +0200

    feat: render data to the countries table

--- src/app/covid-base/components/countries-table/countries-table.component.ts
+++ src/app/covid-base/components/countries-table/countries-table.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ICountries } from 'src/app/core/models/covid-base.models';
 
 @Component({
   selector: 'app-countries-table',
   templateUrl: './countries-table.component.html',
   styleUrls: ['./countries-table.component.scss']
 })
-export class CountriesTableComponent implements OnInit {
-
+export class CountriesTableComponent implements OnInit{
   constructor() { }
+
+  @Input() covidData!: ICountries[];
+  dataSource = new MatTableDataSource<ICountries>(this.covidData);
+  displayedColumns: string[] = ['Country', 'TotalConfirmed'];
 
   ngOnInit(): void {
   }
-
 }
