77d9e86e97b8f2b72c6f69bce047ac779ec08ac0
coriander31415 <coriander31415@gmail.com>
2020-12-23 13:17:46 +0200

    fix: solve merge conflicts

--- src/app/covid-base/components/countries-table/countries-table.component.ts
+++ src/app/covid-base/components/countries-table/countries-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, OnInit} from '@angular/core';
+import { Component, Input, ViewChild, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ICountries } from 'src/app/core/models/covid-base.models';
@@ -8,20 +8,25 @@ import { ICountries } from 'src/app/core/models/covid-base.models';
   templateUrl: './countries-table.component.html',
   styleUrls: ['./countries-table.component.scss']
 })
-export class CountriesTableComponent implements OnInit{
-  @Input() covidData !: ICountries[];
-  dataSource = new MatTableDataSource(this.covidData);
-  displayedColumns = ['Country', 'TotalConfirmed'];
+export class CountriesTableComponent implements OnInit, OnChanges{
+  @Input() Countries!: ICountries[];
+  displayedColumns = ['cases', 'flag', 'country'];
   @ViewChild(MatSort, {static: true}) sort!: MatSort;
+  dataSource!: MatTableDataSource<ICountries>;
 
   constructor() {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.dataSource = new MatTableDataSource(this.Countries);
+    setTimeout(() => this.dataSource.sort = this.sort);
+  }
+
   ngOnInit(): void {
-    this.dataSource.sort = this.sort;
   }
 
   applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log(this.dataSource);
   }
 }
