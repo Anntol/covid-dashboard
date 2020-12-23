6fed19585e420e209e83cc9af46ca4de4eeb5de2
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-12-23 21:46:56 +0300

    fix: init charts

--- src/app/covid-base/components/charts/charts.component.ts
+++ src/app/covid-base/components/charts/charts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Inject, NgZone, PLATFORM_ID } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, AfterViewInit, Input, Inject, NgZone, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 import * as am4core from '@amcharts/amcharts4/core';
@@ -7,25 +7,35 @@ import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 
 import { ICovid19, IHistorical } from '../../../core/models/covid-base.models';
 
-// interface IData {
-//   date: Date,
-//   Value: any,
-// }
+interface IData {
+  date: Date,
+  Value: any,
+}
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.scss']
 })
-export class ChartsComponent implements OnInit {
-  @Input() dataForPrint!: IHistorical;
+export class ChartsComponent implements OnChanges, AfterViewInit {
+  @Input() chartsData!: IHistorical;
+  dataCharts!: IData[];
+
   private chart!: am4charts.XYChart;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: any, private zone: NgZone) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: any, private zone: NgZone) {}
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.chartsData) {
+      if (changes['cases']) {
+        const { cases } = this.chartsData;
+      }
+      // this.dataCharts = cases.call();
+      // console.log(this.dataCharts);
+    }
 
-  // Run the function only in the browser
+  }
+
   browserOnly(f: () => void) {
     if (isPlatformBrowser(this.platformId)) {
       this.zone.runOutsideAngular(() => {
@@ -42,14 +52,14 @@ export class ChartsComponent implements OnInit {
       chart.paddingRight = 20;
 
       let data: any[] = [];
-      console.log(this.dataForPrint);
-      const days = Object.keys(this.dataForPrint);
-      days.forEach(item => {
-        const day =item.split('/');
-        const dayDate = new Date(Number(day[2]), Number(day[0]) - 1, Number(day[1]));
-        const valueDate =  this.dataForPrint[item];
-        data.push({ date: dayDate, value: valueDate});
-      });
+      console.log(this.chartsData);
+      // const days = Object.keys(this.Historical.cases);
+      // days.forEach(item => {
+      //   const day =item.split('/');
+      //   const dayDate = new Date(Number(day[2]), Number(day[0]) - 1, Number(day[1]));
+      //   const valueDate = '';
+      //   data.push({ date: dayDate, value: valueDate});
+      // });
 
       chart.data = data;
       console.log(data);
@@ -76,7 +86,6 @@ export class ChartsComponent implements OnInit {
   }
 
   ngOnDestroy() {
-    // Clean up chart when the component is removed
     this.browserOnly(() => {
       if (this.chart) {
         this.chart.dispose();
