b06c8c90866225710555c8a79c3ae901b7d514fb
coriander31415 <coriander31415@gmail.com>
2020-12-26 16:03:38 +0200

    fix: return back sorting by cases

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IGlobal } from 'src/app/core/models/covid-base.models';
+import { CovidService } from 'src/app/core/services/covid.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  Global!: IGlobal;
+  dateUpdate: any = '';
+  country = 'all';
 
-  constructor() { }
+  constructor(private covidService: CovidService) {}
 
   ngOnInit(): void {
+    this.getAllDataCovid(this.country);
+    console.log(this.getAllDataCovid(this.country));
+  }
+
+  getAllDataCovid(countryName: string): void {
+    this.covidService.getAllDataCovidByParams(countryName).subscribe(() => {
+      this.dateUpdate = new Date (this.Global.updated);
+    });
   }
 
 }
