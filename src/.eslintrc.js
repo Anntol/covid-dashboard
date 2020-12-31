17b12400f251f76b94ee899e45e6d1a51b5e17c0
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-12-31 15:49:37 +0300

    add: migration from tslint to eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: [
+          "tsconfig.*?.json",
+          "e2e/tsconfig.json"
+        ],
+        createDefaultProgram: true
+      },
+      extends: [
+        "plugin:@angular-eslint/recommended",
+        'airbnb-typescript/base',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+      ],
+      rules: {
+        "no-var": "error",
+        "semi": "error",
+        "indent": "error",
+        "no-multi-spaces": "error",
+        "space-in-parens": "error",
+        "no-multiple-empty-lines": "error",
+        "prefer-const": "error",
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'class-methods-use-this': 'off',
+        'lines-between-class-members': 'off',
+        '@typescript-eslint/unbound-method': [
+          'error',
+          {
+            ignoreStatic: true,
+          }
+        ]
+      }
+    },
+    {
+      files: ['src/**/*.spec.ts', 'src/**/*.d.ts'],
+      parserOptions: {
+        project: './src/tsconfig.spec.json',
+      },
+      extends: ['plugin:jasmine/recommended'],
+      plugins: ['jasmine'],
+      env: { jasmine: true },
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+    {
+      files: ["*.component.html"],
+      extends: ["plugin:@angular-eslint/template/recommended"],
+      rules: {
+        "max-len": ["error", { "code": 140 }]
+      }
+    },
+    {
+      files: ["*.component.ts"],
+      extends: ["plugin:@angular-eslint/template/process-inline-templates"]
+    }
+  ]
+}
